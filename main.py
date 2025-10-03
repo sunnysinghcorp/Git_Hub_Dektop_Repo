0eb8cfd08dd4bb40c881d44f8bc660241df97c67
sunny.singh <singh.sunny.corp@gmail.com>
2025-10-03 15:21:31 +0530

    Commiting to UAT branch for c value

---
+++ main.py
@@ -0,0 +1,6 @@
+# This is a simple Python program that takes user input and prints a message.
+a=("I am a Hero : ")
+b=("I am a Super- Hero :")
+c=7
+Value= input("Enter your name : ")
+print([a+Value], [b+Value], [c+value])
