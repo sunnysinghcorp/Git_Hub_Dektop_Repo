b6e72112b05fd573e25d11bdd4f2ab45cf10393c
sunny.singh <singh.sunny.corp@gmail.com>
2025-10-04 11:40:34 +0530

    push to UAT

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 # This is a simple Python program that takes user input and prints a message.
-a=("I am a Hero : ")
-b=("I am a Super- Hero :")
-c=7
-Value= input("Enter your name : ")
-print([a+Value], [b+Value], [c+value])
+a = "I am a Hero: "
+b = "I am a Super-Hero: "
+c = 7
+d = 78.9
+
+# Ask for the user's name (string). Don't cast to float.
+name = input("Enter your name: ")
+
+# Print combined messages. Convert numbers to strings when concatenating.
+print(a + name)
+print(b + name)
+print(str(c) + " " + name)
+print(str(d) + " " + name)
+# Merge the code to UAT branch
+# From test branchto Uat branch
